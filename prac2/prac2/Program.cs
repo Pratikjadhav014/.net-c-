a2d86e661066fec48ecf8542f1411ad665fb3350
pratikjadhav <jadhavpratik9773@gmail.com>
2026-04-10 00:08:34 +0530

    add experiments

---
+++ prac2/prac2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            String name = "jonathan";
+            Console.WriteLine(name);
+            int a = 10;
+            Console.WriteLine(a);
+            char b = 'a';
+            Console.WriteLine(b);
+            double c = 34.56;
+            Console.WriteLine(c);
+            bool d = false;
+            Console.WriteLine(d);
+
+            //constant keyword
+            const int x = 10;
+            //x = 11;//give error ---constant varible does not change after first initilization.
+
+            // + in string 
+
+            //first
+
+            String l = "top";
+            Console.WriteLine("dok" + l);
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
